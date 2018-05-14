24a359e48f3ae4fe34565304c92064a73f74204b
Alejandro Do Nascimento <alejandrodnm@gmail.com>
2018-05-14 07:43:18 +0200

    Add docker-compose file and config

--- truffle.js
+++ truffle.js
@@ -9,6 +9,11 @@ module.exports = {
       host: "localhost",
       port: 8545,
       network_id: "*"
+    },
+    compose: {
+      host: "truffle-develop",
+      port: 9545,
+      network_id: "*"
     }
   }
 };
